be5d2dbacfee33ba16666565edbfbc542ea0a10e
bhishmraj <yogansh37@gmail.com>
2026-06-20 13:24:09 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwYytAzx6fTHz9wAG6D55SLRQQO25mfdQ",
@@ -9,10 +8,9 @@ const firebaseConfig = {
   storageBucket: "swamiwebsite-fc0cc.firebasestorage.app",
   messagingSenderId: "879455002030",
   appId: "1:879455002030:web:7788566b680071640fbc3f",
-  measurementId: "G-VSS8VT3CKT"
+  measurementId: "G-VSS8VT3CKT",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const analytics = getAnalytics(app);
 export default app;
